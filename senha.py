0e3c61f02d245bd1ae01bbda2ec543f8d350fec0
GuiGames <benattimendesgui@gmail.com>
2026-08-04 17:31:59 -0300

    Add files via upload

---
+++ senha.py
@@ -0,0 +1,10 @@
+import random
+
+def senha():
+    caracteres = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '-', '=', '+', '|', ';', ':', ',', '.', '/', '?']
+    pedido = 10
+    senha = ""
+    for i in range(pedido):
+        senha += random.choice(caracteres)
+    return senha
+print(senha())
